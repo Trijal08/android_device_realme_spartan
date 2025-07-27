542dccbd7f14929c6998a0de553513e4dd5347b8
bijoyv9 <bijoyv9@gmail.com>
2025-07-27 11:10:26 +0000

    sm8250-common: Initial rebrand to spartan
    Change-Id: Id53cb2e11ff2a5572c69e6cc64f5e99852662e95

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonades.mk
+    $(LOCAL_DIR)/lineage_spartan.mk
