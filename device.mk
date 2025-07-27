aca1ccfbb770335c7d83fad562a6cd2822318fef
bijoyv9 <bijoyv9@gmail.com>
2025-07-27 11:10:26 +0000

    sm8250-common: Initial rebrand to spartan
    Change-Id: Id53cb2e11ff2a5572c69e6cc64f5e99852662e95

--- device.mk
+++ device.mk
@@ -415,4 +415,4 @@ PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/lemonades/lemonades-vendor.mk)
+$(call inherit-product, vendor/realme/spartan/spartan-vendor.mk)
